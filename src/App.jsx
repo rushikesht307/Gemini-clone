ab089c536500d11c3d0634b86d55717f05a78bd6
rushikesht307 <108972839+rushikesht307@users.noreply.github.com>
2024-05-30 12:20:06 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Main from './components/main/Main'
+import Sidebar from './components/Sidebar/Sidebar'
+
+const App = () => {
+  return (
+    <>
+      <Sidebar/>
+      <Main/>
+    </>
+  )
+}
+
+export default App
